4eaa8cee651545916fdb0a7e327f8835198c26b9
benvanpelt <ben.vanpelt@astral-lcb.com>
2025-05-15 11:21:14 -0400

    Update apl.py

--- apl.py
+++ apl.py
@@ -14,8 +14,8 @@ app = Flask(__name__)
 SUPABASE_URL = "https://hdbfowhiscynicqzqmpn.supabase.co"
 SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhkYmZvd2hpc2N5bmljcXpxbXBuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcyNDE5MDgsImV4cCI6MjA2MjgxNzkwOH0.UKOGJ64EQB1IrGCmPw6jaJHmxdb678Gbnfpca-Zp_YY"
 
-@app.route ("/data", methods = ["GET"])
-def get_data():
+@app.route ("/drops", methods = ["GET"])
+def get_drops():
         headers = {
         "apikey": SUPABASE_KEY,
         "Authorization": f"Bearer {SUPABASE_KEY}"
@@ -28,6 +28,22 @@ def get_data():
                 return jsonify(response.json())
         except Exception as e:
                 return jsonify({"error": str(e)}), 500
+
+@app.route ("/products", methods = ["GET"])
+def get_products():
+        headers = {
+        "apikey": SUPABASE_KEY,
+        "Authorization": f"Bearer {SUPABASE_KEY}"
+            }
+        url = f"{SUPABASE_URL}/rest/v1/apl_products"
+        
+        try:
+                response = requests.get(url, headers=headers)
+                response.raise_for_status()
+                return jsonify(response.json())
+        except Exception as e:
+                return jsonify({"error": str(e)}), 500
+                
 
 if __name__ == "__main__":
     app.run (debug=True, host = "0.0.0.0", port = 5000)
